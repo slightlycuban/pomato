047a210d04f414bd1b2e8fcbc02d8df844cde64a
Michael Tracy <slightlycuban@gmail.com>
2013-10-02 13:44:58 -0400

    Start switching/tracking pom state.

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,14 +1,26 @@
 function PomCtrl($scope, $timeout) {
+  $scope.longbreak = 15;
+  $scope.shortbreak = 5;
+  $scope.pomodoro = 25;
+
+  $scope.state = 0;
+  $scope.pomrun = 3;
+  $scope.poms = 0;
+
   $scope.counter = 25;
 
   $scope.countdown = function() {
     $scope.counter--;
 
     if ($scope.counter > 0) {
-      timer = $timeout($scope.countdown, 1000);
-    } 
+      timer = $timeout($scope.countdown, second);
+    } else {
+      state_update();
+      running = false;
+    }
   }
 
+  var second = 10;
   var timer;
   var running = false;
 
@@ -25,8 +37,28 @@ function PomCtrl($scope, $timeout) {
   
   $scope.start = function() {
     if (!running) {
-      timer = $timeout($scope.countdown, 1000);
+      timer = $timeout($scope.countdown, second);
       running = true;
+    }
+  }
+
+  function state_update() {
+    switch ($scope.state) {
+      case 0:
+        $scope.poms++;
+        if (($scope.poms % $scope.pomrun) === 0) {
+          $scope.state = 2;
+          $scope.counter = $scope.longbreak;
+        } else {
+          $scope.state = 1;
+          $scope.counter = $scope.shortbreak;
+        }
+        break;
+      case 1:
+      case 2:
+        $scope.state = 0;
+        $scope.counter = $scope.pomodoro;
+        break;
     }
   }
 }
