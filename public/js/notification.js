fea083683c9bfe835080dd1e86f018e9a8af9715
Michael Tracy <slightlycuban@gmail.com>
2015-10-26 11:35:57 -0400

    Clean up the name of things in notification.js

--- public/js/notification.js
+++ public/js/notification.js
@@ -7,21 +7,21 @@ document.addEventListener('DOMContentLoaded', function () {
 function sendNotification (title, options) {
   // Memoize based on feature detection.
   if ("Notification" in window) {
-    sendNotification = function (title, options) {
+    notify = function (title, options) {
       new Notification(title, options);
     };
   } else if ("mozNotification" in navigator) {
-    sendNotification = function (title, options) {
+    notify = function (title, options) {
       // Gecko < 22
       navigator.mozNotification
                .createNotification(title, options.body, options.icon)
                .show();
     };
   } else {
-    sendNotification = function (title, options) {
+    notify = function (title, options) {
       alert(title + ": " + options.body);
     };
   }
-  sendNotification(title, options);
-};
+  notify(title, options);
+}
 
