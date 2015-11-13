ae8beeda9e6356247ff254c6a18d785fb47930f3
Michael Tracy <slightlycuban@gmail.com>
2015-11-12 21:48:52 -0500

    Move some properties to use controller-as syntax
    I'll get to the rest later

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -3,30 +3,31 @@ angular.module('pomato', [])
 .controller('PomCtrl', ['$scope', '$timeout', PomCtrl]);
 
 function PomCtrl($scope, $timeout) {
+  var self = this;
   $scope.longbreak = 15;
   $scope.shortbreak = 5;
   $scope.pomodoro = 25;
 
-  $scope.state = PomEnum.Pomodoro;
+  self.state = PomEnum.Pomodoro;
   $scope.pomrun = 3;
   $scope.poms = 0;
 
-  $scope.counter = $scope.pomodoro * 60;
+  self.counter = $scope.pomodoro * 60;
   $scope.running = false;
 
   var time;
 
-  $scope.countdown = function() {
+  self.countdown = function() {
     var now = new Date().getTime();
     var elapsed = now - time;
-    $scope.counter -= (elapsed / second);
+    self.counter -= (elapsed / second);
     time = now;
 
-    if ($scope.counter > 0) {
-      timer = $timeout($scope.countdown, second);
+    if (self.counter > 0) {
+      timer = $timeout(self.countdown, second);
     } else {
       state_update();
-      sendNotification("Time is up", { body: $scope.state + " time now"});
+      sendNotification("Time is up", { body: self.state + " time now"});
       $scope.running = false;
     }
   };
@@ -34,64 +35,64 @@ function PomCtrl($scope, $timeout) {
   var second = 1000;
   var timer;
 
-  $scope.reset = function() {
-    $scope.pause();
-    switch ($scope.state) {
+  self.reset = function() {
+    self.pause();
+    switch (self.state) {
       case PomEnum.Pomodoro:
-        $scope.counter = $scope.pomodoro;
+        self.counter = $scope.pomodoro;
         break;
       case PomEnum.Short:
-        $scope.counter = $scope.shortbreak;
+        self.counter = $scope.shortbreak;
         break;
       case PomEnum.Long:
-        $scope.counter = $scope.longbreak;
+        self.counter = $scope.longbreak;
         break;
     }
-    $scope.counter = $scope.counter * 60;
+    self.counter = self.counter * 60;
   };
   
-  $scope.pause = function() {
+  self.pause = function() {
     if ($scope.running)
       $scope.running = !$timeout.cancel(timer);
   };
   
-  $scope.start = function() {
+  self.start = function() {
     if (!$scope.running) {
       time = new Date().getTime();
-      timer = $timeout($scope.countdown, second);
+      timer = $timeout(self.countdown, second);
       $scope.running = true;
     }
   };
 
-  $scope.minutes = function() {
-    //return Math.floor($scope.counter / 60)
-    return padzero(~~($scope.counter / 60), 2);
+  self.minutes = function() {
+    //return Math.floor(self.counter / 60)
+    return padzero(~~(self.counter / 60), 2);
   };
 
-  $scope.seconds = function() {
-    return padzero(~~($scope.counter % 60), 2);
+  self.seconds = function() {
+    return padzero(~~(self.counter % 60), 2);
   };
 
   function state_update() {
-    switch ($scope.state) {
+    switch (self.state) {
       case PomEnum.Pomodoro:
         $scope.poms++;
         if (($scope.poms % $scope.pomrun) === 0) {
-          $scope.state = PomEnum.Long;
-          $scope.counter = $scope.longbreak;
+          self.state = PomEnum.Long;
+          self.counter = $scope.longbreak;
         } else {
-          $scope.state = PomEnum.Short;
-          $scope.counter = $scope.shortbreak;
+          self.state = PomEnum.Short;
+          self.counter = $scope.shortbreak;
         }
         break;
       case PomEnum.Short:
       case PomEnum.Long:
-        $scope.state = PomEnum.Pomodoro;
-        $scope.counter = $scope.pomodoro;
+        self.state = PomEnum.Pomodoro;
+        self.counter = $scope.pomodoro;
         break;
     }
 
-    $scope.counter = $scope.counter * 60;
+    self.counter = self.counter * 60;
   }
 }
 
