243f3bd50693082910d71e29b91e8fa63e80e6a4
Michael Tracy <slightlycuban@gmail.com>
2015-11-21 14:52:38 -0500

    Break code down into services and controllers

--- js/app.js
+++ js/app.js
@@ -1,99 +1,139 @@
 (function() {
 angular.module('pomato', [])
-.controller('PomCtrl', ['$timeout', PomCtrl]);
+.constant('config', { refresh: 1000 })
+.value('millisPer', { second: 1000, minute: 60 * 1000 })
+.factory('SettingsSvc', [SettingsSvc])
+.factory('TimerSvc', ['$timeout', 'SettingsSvc', 'millisPer', 'config', TimerSvc])
+.controller('PomCtrl', ['SettingsSvc', 'TimerSvc', 'millisPer', PomCtrl]);
 
-function PomCtrl($timeout) {
-  var self = this;
-  self.longbreak = 15;
-  self.shortbreak = 5;
-  self.pomodoro = 25;
+function SettingsSvc() {
+  return {
+    longbreak: 15,
+    shortbreak: 5,
+    pomodoro: 25,
+    pomrun: 3
+  };
+}
 
+function TimerSvc($timeout, settings, millisPer, config) {
+  var self = {};
+
+  // Pomato state
   self.state = PomEnum.Pomodoro;
-  self.pomrun = 3;
   self.poms = 0;
 
-  self.counter = self.pomodoro * 60;
+  // Timer
+  self.counter = minutesToMillis(settings.pomodoro);
   self.running = false;
+
+  // Actions
+  self.reset = reset;
+  self.pause = pause;
+  self.start = start;
 
   var time;
 
-  self.countdown = function() {
+  function countdown() {
     var now = new Date().getTime();
     var elapsed = now - time;
-    self.counter -= (elapsed / second);
+    self.counter -= elapsed;
     time = now;
 
     if (self.counter > 0) {
-      timer = $timeout(self.countdown, second);
+      timer = $timeout(countdown, config.refresh);
     } else {
       state_update();
       sendNotification("Time is up", { body: self.state + " time now"});
       self.running = false;
     }
-  };
+  }
 
-  var second = 1000;
   var timer;
 
-  self.reset = function() {
+  function reset() {
     self.pause();
     switch (self.state) {
       case PomEnum.Pomodoro:
-        self.counter = self.pomodoro;
+        self.counter = minutesToMillis(settings.pomodoro);
         break;
       case PomEnum.Short:
-        self.counter = self.shortbreak;
+        self.counter = minutesToMillis(settings.shortbreak);
         break;
       case PomEnum.Long:
-        self.counter = self.longbreak;
+        self.counter = minutesToMillis(settings.longbreak);
         break;
     }
-    self.counter = self.counter * 60;
-  };
+  }
   
-  self.pause = function() {
-    if (self.running)
+  function pause() {
+    if (self.running) {
       self.running = !$timeout.cancel(timer);
-  };
+    }
+  }
   
-  self.start = function() {
+  function start() {
     if (!self.running) {
       time = new Date().getTime();
-      timer = $timeout(self.countdown, second);
+      timer = $timeout(countdown, config.refresh);
       self.running = true;
     }
-  };
-
-  self.remaining = function() {
-    return padzero(~~(self.counter / 60), 2) +
-      ":" +
-      padzero(~~(self.counter % 60), 2);
-  };
-
-  self.title = function () {
-    return self.remaining() + " remaing";
-  };
+  }
 
   function state_update() {
     switch (self.state) {
       case PomEnum.Pomodoro:
         self.poms++;
-        if ((self.poms % self.pomrun) === 0) {
+        if ((self.poms % settings.pomrun) === 0) {
           self.state = PomEnum.Long;
-          self.counter = self.longbreak;
+          self.counter = minutesToMillis(settings.longbreak);
         } else {
           self.state = PomEnum.Short;
-          self.counter = self.shortbreak;
+          self.counter = minutesToMillis(settings.shortbreak);
         }
         break;
       case PomEnum.Short:
       case PomEnum.Long:
         self.state = PomEnum.Pomodoro;
-        self.counter = self.pomodoro;
+        self.counter = minutesToMillis(settings.pomodoro);
         break;
     }
+  }
 
-    self.counter = self.counter * 60;
+  function minutesToMillis(minutes) {
+    return minutes * millisPer.minute;
+  }
+
+  return self;
+}
+
+function PomCtrl(settings, timer, millisPer) {
+  var self = this;
+  // Settings
+  self.settings = settings;
+
+  self.timer = timer;
+
+  // Display
+  self.remaining = remaining;
+  self.title = title;
+
+  function remaining() {
+    var secondsRemaining = timer.counter / millisPer.second;
+    return padzero(~~(secondsRemaining / 60), 2) +
+      ":" +
+      padzero(~~(secondsRemaining % 60), 2);
+  }
+
+  function title() {
+    return remaining() + " remaing";
+  }
+
+  function padzero(number, width) {
+    var str = number + "";
+    while (str.length < width) {
+      str = "0" + str;
+    }
+    return str;
   }
 }
 
@@ -102,12 +142,4 @@ var PomEnum = {
   Short: "Short Break",
   Long: "Long Break",
 };
-
-function padzero(number, width) {
-  var str = number + "";
-  while (str.length < width) {
-    str = "0" + str;
-  }
-  return str;
-}
 })();
